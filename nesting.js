4ed46e9a08d53c4cfffb3a5006f8f689460e6a7f
david collett <davida.collett95@gmail.com>
2019-06-28 20:22:01 -0600

    done

--- nesting.js
+++ nesting.js
@@ -159,22 +159,16 @@ var myCar = {
     3. Change atFaultForAccident from true to false.
 */
 
-// function recordCleaner() {
-//   for (let i = 0 ; myCar['accidents'].length ; i++) {
-//     for (let key in myCar['accidents'][i]) {
-//       if (myCar['accidents'][i][key] === true) {
-//         myCar['accidents'][i]['atFaultForAccident'] = false
-//         }
-//       }
-        
-//     }
-//     return myCar
-  // }
 function recordCleaner() {
-  for (let key in myCar) {
-    for (let i = 0)
+  for (let i = 0 ; i < myCar['accidents'].length ; i++) {
+    for (let key in myCar['accidents'][i]) {
+      if (key === 'atFaultForAccident') {
+        myCar['accidents'][i][key] = false
+        }
+      }    
+    }
+    return myCar
   }
-}
 
 
 ////////// PROBLEM 5 //////////
@@ -192,6 +186,16 @@ var numsArr = [ [1, 2, 3, 4], [5, 6], [7, 8, 9, 10, 11]];
     4. Return the modified numsArr.
 */
 
-//Code Here
-
-
+function looper() {
+  for (let i = 0 ; i < numsArr.length ; i++) {
+    for (let j = 0 ; j < numsArr[i].length ; j++) {
+      if (numsArr[i][j] % 2 === 0) {
+        numsArr[i][j] = 'even'
+      }
+      else {
+        numsArr[i][j] = 'odd'
+      }
+    }
+  }
+  return numsArr
+}
